8170addd4871c40852a47bef08cc942b1f196e19
john <wildfireone@gmail.com>
2019-03-05 18:16:34 +0000

    final before

--- src/flashing.js
+++ src/flashing.js
@@ -11,12 +11,15 @@
 
 //set a value
  const msFrequency = 200;
+
+ //clear the matrix before we start
+ matrix.clear();
+
 //sets up some code to repeat at a given interval
  streamInterval = setInterval(working, msFrequency);
 
 //the function to perfrom!
  function working(){
-
 
      const x = returnRandom(0,8);
      const y = returnRandom(0,8);
@@ -24,11 +27,9 @@
 
      // Set a single pixel
      matrix.setPixel(x, y, red);
-
-
  }
 
 //function to return a random number
  function returnRandom(min, max) {
-   return (Math.random() * (max - min) + min).toFixed(0);
+   return parseInt(Math.random() * (max - min) + min).toFixed(0));
  }
