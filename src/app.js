e05872c938a9cd92162effb180fb476f2af333a5
john <wildfireone@gmail.com>
2019-03-05 11:11:29 +0000

    initial setup

--- src/app.js
+++ src/app.js
@@ -1,93 +1,88 @@
-const express = require('express'),
-    app = express(),
-    cors = require('cors'),
-    morgan = require('morgan'),
-    path = require('path'),
-    bodyParser = require('body-parser'),
-    mqtt = require('mqtt'),
-    mqttClient = mqtt.connect('mqtt://0.0.0.0:1883'),
-    mqttTopic = 'voltage',
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server);
+/**
+ * @Author: John Isaacs <john>
+ * @Date:   04-Mar-192019
+ * @Filename: app.js
+ * @Last modified by:   john
+ * @Last modified time: 05-Mar-192019
+ */
+
+
+
+const mqtt = require('mqtt'),
+  mqttClient = mqtt.connect('mqtt://0.0.0.0:1883'),
+  mqttTopic = '/inbox/'+devicename+'/deviceInfo'
 
 var streamInterval;
-var msFrequency = 20;
+var msFrequency = 200;
+var devicename = "testDevice"
+var topic = "temperature"
 
-/* 
+/*
 Subscribe (listen) to MQTT topic and start publishing
-simulated data after successful MQTT connection 
+simulated data after successful MQTT connection
 */
 mqttClient.on('connect', () => {
-    console.log('Mqtt connected.')
-    mqttClient.subscribe(mqttTopic); //subscribe
-    startStreamSimulation(); //publish
+  console.log('Mqtt connected.')
+  mqttClient.subscribe('/inbox/' + devicename + '/deviceInfo'); //subscribe
+  mqttClient.subscribe('/inbox/' + devicename + '/' + topic); //subscribe
+  mqttClient.publish('/outbox/' + devicename + '/deviceInfo', JSON.stringify({
+    "deviceInfo": {
+      "name": devicename,
+      "endPoints": {
+        "temperature": {
+          "title": "Sensor Temp",
+          "card-type": "crouton-simple-text",
+          "units": "people entered",
+          "values": {
+            "lables": [],
+            "series": [76],
+          },
+          "total": 100,
+          "centerSum": true,
+          "units": "c",
+          "card-type": "crouton-chart-donut",
+          "title": "Temperature"
+        }
+      },
+      "description": "Johns test device",
+      "status": "good"
+    }
+  }));
+  startStreamSimulation(); //publish
 })
 
 mqttClient.on('offline', () => {
-    console.log('Mqtt offline.')
-    mqttClient.unsubscribe(mqttTopic);
-    clearInterval(streamInterval);
+  console.log('Mqtt offline.')
+  mqttClient.unsubscribe('/outbox/' + devicename + '/' + topic);
+  clearInterval(streamInterval);
 })
 
-/* 
-Message event fires, when new messages
-arrive on the subscribed topic
-*/
-mqttClient.on('message', function (topic, message) {
-    /* console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString()); */
-    let parsedMessage = JSON.parse(message);
-    io.emit('voltageData', parsedMessage);
-})
 
-/* 
+/*
 Function that publishes simulated data to the MQTT broker every ≈20ms
 */
 function startStreamSimulation() {
-    
-    var v1 = 0,
-        v2 = 0,
-        v3 = 0;
 
-    streamInterval = setInterval(function () {
+  var v1 = 0,
+    v2 = 0,
+    v3 = 0;
 
-        /* Prepare random data */
-        v1 = returnRandomFloat(231, 231.1);
-        v2 = returnRandomFloat(235, 235.3);
-        v3 = returnRandomFloat(238.7, 239.3);
+  streamInterval = setInterval(function() {
 
-        /* Publish random data to the corresponding MQTT topic as a JSON string  */
-        mqttClient.publish(mqttTopic, JSON.stringify({
-            'v1': v1,
-            'v2': v2,
-            'v3': v3
-        }));
+    /* Prepare random data */
+    v1 = returnRandomFloat(30,50 );
+    v2 = returnRandomFloat(235, 235.3);
+    v3 = returnRandomFloat(238.7, 239.3);
+
+    /* Publish random data to the corresponding MQTT topic as a JSON string  */
+    mqttClient.publish('/outbox/' + devicename + '/' + topic, JSON.stringify({
+      "series": [v1]
+    }));
 
 
-    }, msFrequency);
+  }, msFrequency);
 }
 
 function returnRandomFloat(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(2);
+  return (Math.random() * (max - min) + min).toFixed(2);
 }
-
-io.on('connection', (client) => {
-    console.log("Socket connected.")
-})
-
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-    extended: true
-}));
-
-app.get('/', function (req, res) {
-    res.send(
-    [{
-            title: "Hi, I'm the express server!",
-            description: "Start Moquette and the client application to see the action!"
-    }]
-    )
-});
-
-server.listen(3000, function () {
-    console.log('App listening on port 3000!');
-});
