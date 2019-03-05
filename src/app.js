faac00e2ff7400b87a8b6403c7ef2785fef56fb7
john <wildfireone@gmail.com>
2019-03-05 11:50:06 +0000

    tidying up mqtt code

--- src/app.js
+++ src/app.js
@@ -7,76 +7,67 @@
  */
 
 
+//these two lines set up the connection to our displaye server
+const mqtt = require('mqtt')
+const mqttClient = mqtt.connect('mqtt://0.0.0.0:1883')
 
-const mqtt = require('mqtt'),
-  mqttClient = mqtt.connect('mqtt://0.0.0.0:1883'),
-  mqttTopic = '/inbox/'+devicename+'/deviceInfo'
+//these two lines define our devices name and a topic for the display server
+const devicename = "testDevice"
+const topic = "temperature"
 
+//these lines set up some infromation that is needed by the server,
+//this is just so we dont have to keep typing them in
+var deviceInfoIn = '/inbox/' + devicename + '/deviceInfo';
+var deviceInfoOut = '/outbox/' + devicename + '/deviceInfo';
+var deviceTelemetery = '/outbox/' + devicename + '/deviceInfo';
+
+//here we set how fast the stream is, (how often the data is pushed to the display server)
 var streamInterval;
 var msFrequency = 200;
-var devicename = "testDevice"
-var topic = "temperature"
 
 /*
-Subscribe (listen) to MQTT topic and start publishing
-simulated data after successful MQTT connection
+This bloc of code sets up the type of dispay we will see on the server and starts the connection
 */
 mqttClient.on('connect', () => {
   console.log('Mqtt connected.')
-  mqttClient.subscribe('/inbox/' + devicename + '/deviceInfo'); //subscribe
-  mqttClient.subscribe('/inbox/' + devicename + '/' + topic); //subscribe
-  mqttClient.publish('/outbox/' + devicename + '/deviceInfo', JSON.stringify({
+  mqttClient.subscribe(deviceInfoIn); //subscribe
+  mqttClient.subscribe(deviceTelemetery); //subscribe
+  mqttClient.publish(deviceInfoOut, JSON.stringify({
     "deviceInfo": {
       "name": devicename,
       "endPoints": {
         "temperature": {
           "title": "Sensor Temp",
           "card-type": "crouton-simple-text",
-          "units": "people entered",
+          "units": "C",
           "values": {
-            "lables": [],
-            "series": [76],
+            "value": 39
           },
-          "total": 100,
-          "centerSum": true,
-          "units": "c",
-          "card-type": "crouton-chart-donut",
-          "title": "Temperature"
         }
       },
       "description": "Johns test device",
       "status": "good"
     }
   }));
-  startStreamSimulation(); //publish
+  startStream(); //publish
 })
-
-mqttClient.on('offline', () => {
-  console.log('Mqtt offline.')
-  mqttClient.unsubscribe('/outbox/' + devicename + '/' + topic);
-  clearInterval(streamInterval);
-})
-
 
 /*
-Function that publishes simulated data to the MQTT broker every ≈20ms
+This function controls sending data to the server
 */
-function startStreamSimulation() {
+function startStream() {
+  //set up a variable for the temperature
+  var temp = 0;
 
-  var v1 = 0,
-    v2 = 0,
-    v3 = 0;
 
   streamInterval = setInterval(function() {
 
     /* Prepare random data */
-    v1 = returnRandomFloat(30,50 );
-    v2 = returnRandomFloat(235, 235.3);
-    v3 = returnRandomFloat(238.7, 239.3);
+    temp = returnRandomFloat(30, 50);
 
-    /* Publish random data to the corresponding MQTT topic as a JSON string  */
-    mqttClient.publish('/outbox/' + devicename + '/' + topic, JSON.stringify({
-      "series": [v1]
+    /* Publish data to the display server */
+    mqttClient.publish(deviceTelemetery, JSON.stringify({
+      "value": temp
     }));
 
 
@@ -86,3 +77,10 @@ function startStreamSimulation() {
 function returnRandomFloat(min, max) {
   return (Math.random() * (max - min) + min).toFixed(2);
 }
+
+//this block of code handles disconnecting
+mqttClient.on('offline', () => {
+  console.log('Mqtt offline.')
+  mqttClient.unsubscribe(deviceTelemetery);
+  clearInterval(streamInterval);
+})
